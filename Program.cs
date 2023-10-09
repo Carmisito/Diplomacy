95b7a54a441d764cb4a55586d10a1f85b22adfab
Carmisito <58825188+Carmisito@users.noreply.github.com>
2023-10-09 23:40:52 +0300

    Update Program.cs
    added a lot of the infrastructure (boilerplate)

--- Program.cs
+++ Program.cs
@@ -1,11 +1,14 @@
-﻿internal class Program
+using System.Collections.Generic;
+internal class Program
 {
+
+    const TerritoryLength = Enum.getNames(typeof(TerritoryName)).Length;
 
     public readonly new HashSet<HashSet<TerritoryName>> Connections = new HashSet<HashSet<TerritoryName>>{new HashSet<TerritoryName> {TerritoryName.Poop, TerritoryName.Butt},
                                                                                                           new HashSet<TerritoryName> {TerritoryName.Poop, TerritoryName.Peepee}
     };
 
-    public enum Country{
+    public readonly enum Country{
         Russia,
         Germany,
         Austria,
@@ -16,21 +19,22 @@
         None
     }
 
-    public enum TerritoryType{
+    public readonly enum TerritoryType{
         Land,
         Sea,
         SupplyCenter,
         StartingSupplyCenter
     }
 
-    public enum MoveAction{
+    public readonly enum MoveAction{
+        Convoy,
         Hold,
         Move,
-        Support,
-        Convoy
+        Support
+        
     }
 
-    public enum TerritoryName{
+    public readonly enum TerritoryName{
         Poop,
         Butt,
         Peepee
@@ -46,6 +50,15 @@
             this.origin = origin;
             this.target = target;
         }
+
+        public void GetAction(){
+            return this.action;
+        }
+
+        public Move clone(){
+            return new Move(this.action, this.origin, this.target);
+        }
+
     }
 
     class Territory {
@@ -65,12 +78,69 @@
             this.move = new Move(MoveAction.Hold, name, name);
         }
 
+        //Constructor with Move input
+        public Territory(TerritoryName name, Country occupation, Country ownership, TerritoryType territoryType, Move move){
+            this.name = name;
+            this.occupation = occupation;
+            this.ownership = ownership;
+            this.territoryType = territoryType;
+            this.move = move;
+        }
+
         public TerritoryName getName(){
             return this.name;
         }
 
+        public TerritoryName getMove(){
+            return this.move;
+        }
+
+        public Territory clone(){
+            return new Territory(this.name, this.occupation, this.ownership, this.territoryType, this.move.clone());
+        }
+
     }
 
+
+    //This function deep clones a Territory List
+    public List<Territory> CloneTerritoryList(List<Territory> territories){
+        int counter = 0;
+        List<Territory> ret = new List<Territory>();
+        for(counter = 0; counter < territories.Count; counter++){
+            ret.Add(territories[counter].clone());
+        }
+        return ret;
+    }
+
+    public Class CheckClass{
+
+
+
+        public static (List<string> log, List<Territory> newState) CheckTerritory(List<Territory> oldTerritories){
+              List<string> log = new List<string>();
+              List<Territory> newState = new List<Territory>();
+              List<Territory> territories = CloneTerritoryList(oldTerritories);
+              territories.Sort((ter1, ter2) => ter1.getMove().GetAction() - ter2.getMove().GetAction()); //sorts the list so that the convoys are checked first
+
+              While(newState.Count != TerritoryLength){
+                
+                  switch(territories[0]){
+                  
+                      case MoveAction.Convoy:
+                        
+                      break;
+                      case MoveAction.Hold:
+                          
+                          break; //fuck you
+                      case MoveAction.Move:
+                          
+                          break;
+                    }  
+                }
+
+        }
+
+    }
 
 
     private static void Main(string[] args)
